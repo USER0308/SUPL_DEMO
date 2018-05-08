d58a5ea8642d76e3997f074220b0173866fa6662
djj <Administrator@WIN-MDK1PPDCNPU>
2018-05-08 14:32:28 +0800

    add somme Utils method

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -106,17 +106,26 @@ public class Utils {
     * @author wangbin
     */
    public static String fileRead(String filePath) throws Exception {
-       File file = new File(filePath);//定义一个file对象，用来初始化FileReader
-       FileReader reader = new FileReader(file);//定义一个fileReader对象，用来初始化BufferedReader
-       BufferedReader bReader = new BufferedReader(reader);//new一个BufferedReader对象，将文件内容读取到缓存
-       StringBuilder sb = new StringBuilder();//定义一个字符串缓存，将字符串存放缓存中
+       File file = new File(filePath);						//定义一个file对象，用来初始化FileReader
+       FileReader reader = new FileReader(file);			//定义一个fileReader对象，用来初始化BufferedReader
+       BufferedReader bReader = new BufferedReader(reader);	//new一个BufferedReader对象，将文件内容读取到缓存
+       StringBuilder sb = new StringBuilder();				//定义一个字符串缓存，将字符串存放缓存中
        String s = "";
-       while ((s =bReader.readLine()) != null) {//逐行读取文件内容，不读取换行符和末尾的空格
-           sb.append(s + "\n");//将读取的字符串添加换行符后累加存放在缓存中
+       while ((s =bReader.readLine()) != null) {			//逐行读取文件内容，不读取换行符和末尾的空格
+           sb.append(s + "\n");								//将读取的字符串添加换行符后累加存放在缓存中
        }
        bReader.close();
        String str = sb.toString();
        return str;
    }
-
+   
+   
+   /**
+    * simpleDateFormat统一格式化时间
+    */
+   public static String sdf(Object object) {
+	   SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	   return simpleDateFormat.format(object);
+   }
+   
 }
