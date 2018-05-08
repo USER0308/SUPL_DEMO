80b07388450b70fda1d573198881c997881a7c52
djj <Administrator@WIN-MDK1PPDCNPU>
2018-05-08 14:38:09 +0800

    update some thing

--- src/com/formssi/controller/FileController.java
+++ src/com/formssi/controller/FileController.java
@@ -1,7 +1,9 @@
 package com.formssi.controller;
 
+import java.io.File;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.formssi.entity.ReturnJson;
 import com.formssi.entity.ShareFile;
 import com.formssi.service.impl.FileServiceImpl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+
+import utils.Utils;
 
 @Controller
 //@RequestMapping("/login")
@@ -25,16 +30,42 @@ public class FileController {
  
 	@RequestMapping(value = "/uploadFile", produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public String uploadFile(@RequestParam("data") String date, HttpServletResponse response) {
+	public String uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", "*");//跨域访问
 		
-		ShareFile shareFile = ShareFile.parse(date);
-		
-		ReturnJson returnJson = new ReturnJson();
-		
-		if (null == shareFile || "".equals(shareFile.getFileId()) ) {
+		String documentFileName = file.getOriginalFilename();
+    	String extension = documentFileName.substring(documentFileName.lastIndexOf("."));//获取文件后缀
+    	String path=Thread.currentThread().getContextClassLoader().getResource("").getPath()+"\\files\\";//获取要写入的文件路径
+    	System.out.println(path);
+    	
+    	String fileName = path+String.valueOf(System.currentTimeMillis()) + extension;//拼装文件路径和名字
+    	System.out.println(fileName);
+        
+    	ReturnJson returnJson = new ReturnJson();
+    	
+    	try {
+    		//从request中获取上传的文件，然后写入到目标文件中
+			file.transferTo(new File(fileName));
+		}  catch (Exception e1) {
 			returnJson.setSuccess(false);
-			returnJson.setMessage("文件不存在！");
+			returnJson.setMessage("文件上传失败！");
+			return returnJson.toJSON();
+		}
+    	
+    	ShareFile shareFile = new ShareFile();
+    	String fileId = request.getParameter("fileId");
+    	shareFile.setFileId(fileId);
+    	shareFile.setFileAddr(request.getParameter("fileAddr"));
+    	shareFile.setPubKeyToSymkey(request.getParameter("pubKeyToSymkey"));
+    	shareFile.setAllowDep(request.getParameter("allowDep"));
+    	shareFile.setAllowRank(Integer.parseInt(request.getParameter("allowRank")));
+    	shareFile.setDepartment(Integer.parseInt(request.getParameter("department")));
+    	shareFile.setDescription(request.getParameter("description"));
+    	shareFile.setUserId(request.getParameter("userId"));
+		
+		if (null == shareFile || Utils.stringIsNull(fileId) ) {
+			returnJson.setSuccess(false);
+			returnJson.setMessage("文件号不能为空！");
 			return returnJson.toJSON();
 		}
 		
@@ -60,7 +91,7 @@ public class FileController {
 		
 		ReturnJson returnJson = new ReturnJson();
 		
-		if (null == shareFile || "".equals(shareFile.getFileId()) ) {
+		if (null == shareFile || Utils.stringIsNull(shareFile.getFileId()) ) {
 			returnJson.setSuccess(false);
 			returnJson.setMessage("文件不存在！");
 			return returnJson.toJSON();
@@ -73,6 +104,40 @@ public class FileController {
 		}catch(Exception e) {
 			returnJson.setSuccess(false);
 			returnJson.setMessage("文件下载失败！");
+			return returnJson.toJSON();
+		}
+		
+		return returnJson.toJSON();
+	}
+	
+	@RequestMapping(value = "/checkFile", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String checkFile(@RequestParam("data") String date, HttpServletResponse response) {
+		response.setHeader("Access-Control-Allow-Origin", "*");//跨域访问
+		
+		ShareFile shareFile = ShareFile.parse(date);
+		
+		ReturnJson returnJson = new ReturnJson();
+		
+		if (null == shareFile || Utils.stringIsNull(shareFile.getFileId()) ) {
+			returnJson.setSuccess(false);
+			returnJson.setMessage("文件号不能为空！");
+			return returnJson.toJSON();
+		}
+		
+		try {
+			ShareFile shareFileResult = fileService.getById(shareFile.getFileId());
+			if(shareFileResult == null) {
+				returnJson.setSuccess(true);
+				returnJson.setMessage("文件检查成功，无重复文件！");
+			}else {
+				returnJson.setSuccess(false);
+				returnJson.setMessage("文件检查失败，文件已存在！");
+			}
+			
+		}catch(Exception e) {
+			returnJson.setSuccess(false);
+			returnJson.setMessage("文件检查失败！");
 			return returnJson.toJSON();
 		}
 		
--- src/com/formssi/controller/UserController.java
+++ src/com/formssi/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.formssi.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +15,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.formssi.entity.ReturnJson;
 import com.formssi.entity.User;
 import com.formssi.service.impl.UserServiceImpl;
+import com.sun.jmx.snmp.Timestamp;
 
 import utils.Base64Utils;
+import utils.MD5Util;
 import utils.Token;
 import utils.Utils;
 
@@ -25,7 +31,7 @@ public class UserController {
 	
 	@RequestMapping(value = "/login", produces = "application/json;charset=UTF-8")
 	@ResponseBody
-	public String login(@RequestParam("data") String date, HttpServletResponse response) {
+	public String login(@RequestParam("data") String date, HttpServletResponse response, HttpSession session) {
 		response.setHeader("Access-Control-Allow-Origin", "*");//跨域访问
 		
 		User userInput = User.parse(date);
@@ -36,16 +42,22 @@ public class UserController {
 			//通过userService去数据库查该id的用户对象。
 			User user = userService.getById(userInput.getUserId());
 			//查到映射成的对象如果为空，表示该用户不存在
-			if ("".equals(user) || null==user) {
+			if (null == user || Utils.stringIsNull(user.getUserId())) {
 				returnJson.setSuccess(false);
 				returnJson.setMessage("用户不存在！");
 				return returnJson.toJSON();
-			}else if (!Utils.stringIsNull(userInput.getPassword()) && Base64Utils.encode(userInput.getPassword().getBytes()).equals(user.getPassword())) {
+			}else if (!Utils.stringIsNull(userInput.getPassword()) && MD5Util.MD5Encode(userInput.getPassword(), "UTF-8").equals(user.getPassword())) {
 				//用户密码不为空，且密码一致。登录成功
 				returnJson.setSuccess(true);
 				returnJson.setMessage("登录成功！");
 				//登录成功返回token
-				returnJson.setObj(Token.getToken());
+				String token = Token.getToken();
+				returnJson.setObj(token);
+				
+				//带时间的token存入session，便于拦截器失效校验
+				Map<String, Long> map = new HashMap<>();
+				map.put(token, new Timestamp().getDateTime());
+				session.setAttribute("token", map);
 			}else {//其他情况一律登录失败
 				returnJson.setSuccess(false);
 				returnJson.setMessage("用户名或密码错误！");
@@ -68,15 +80,15 @@ public class UserController {
 		
 		ReturnJson returnJson = new ReturnJson();
 		
-		if (null == user || "".equals(user.getUserId()) || "".equals(user.getPassword()) ) {
+		if (null == user || Utils.stringIsNull(user.getUserId()) || Utils.stringIsNull(user.getPassword()) ) {
 			returnJson.setSuccess(false);
-			returnJson.setMessage("用户不存在！");
+			returnJson.setMessage("用户数据不能为空！");
 			return returnJson.toJSON();
 		}
 		
 		try {
-			//密码不存明文，存编码后的值
-			user.setPassword(Base64Utils.encode(user.getPassword().getBytes()));
+			//密码不存明文，存MD5加密后的值
+			user.setPassword(MD5Util.MD5Encode(user.getPassword(), "UTF-8"));
 			
 			userService.add(user);
 			returnJson.setSuccess(true);
@@ -84,6 +96,40 @@ public class UserController {
 		}catch(Exception e) {
 			returnJson.setSuccess(false);
 			returnJson.setMessage("添加用户失败！");
+			return returnJson.toJSON();
+		}
+		
+		return returnJson.toJSON();
+	}
+	
+	@RequestMapping(value = "/checkUser", produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String checkUser(@RequestParam("data") String date, HttpServletResponse response) {
+		response.setHeader("Access-Control-Allow-Origin", "*");//跨域访问
+		
+		User user = User.parse(date);
+		
+		ReturnJson returnJson = new ReturnJson();
+		
+		if (null == user || Utils.stringIsNull(user.getUserId())) {
+			returnJson.setSuccess(false);
+			returnJson.setMessage("用户编号不能为空！");
+			return returnJson.toJSON();
+		}
+		
+		try {
+			User userResult = userService.getById(user.getUserId());
+			if(userResult == null) {
+				returnJson.setSuccess(true);
+				returnJson.setMessage("检查用户成功，无重复用户！");
+			}else {
+				returnJson.setSuccess(false);
+				returnJson.setMessage("检查用户失败，用户已存在！");
+			}
+			
+		}catch(Exception e) {
+			returnJson.setSuccess(false);
+			returnJson.setMessage("检查用户失败！");
 			return returnJson.toJSON();
 		}
 		
